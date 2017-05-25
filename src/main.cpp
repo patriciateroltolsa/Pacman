50a75d3949499b2a1479804f4976583e79d0c213
ekdud014 <ekdud014@naver.com>
2017-05-25 19:25:13 +0900

    Update main.cpp
    Modified the spacing of comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,19 @@
 	3D & 2D Pacman
 	Original: https://github.com/patriciateroltolsa/Pacman
 	Updates: https://github.com/ekdud014/OSS_pacman
+	
 	Original Copyright (C) Patricia Terol
 	Updates Copyright (C) Yeji Na, Dayoung Park, Sojeong Lee, Seungyeon Lee
+	
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, version 3.
+	
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
+	
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
